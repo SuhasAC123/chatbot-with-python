a46a3f0e14a8db90a302375b4161d91d4877f3f9
SuhasAC123 <101121167+SuhasAC123@users.noreply.github.com>
2022-10-29 10:18:21 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,75 @@
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is kira, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*)",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*)created(.*)",
+        ["SUHAS.A.C created me using Python's NLTK library,top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Bangalore, India',]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+    ],
+    [
+        r"how (.*) health",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who (.*) (Cricketer|Batsman)?",
+        ["Virat Kohli"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [
+        r"which is your faverate (.*)",
+        ['there are many :)']
+    ],
+]
+#default message at the start of chat
+print("Hi, I'm kira and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+#Create Chat Bot
+chat = Chat(pairs, reflections)
+#Start conversation
+chat.converse()
